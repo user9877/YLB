b21e891cc5924bdfa7ac1c67bd4fe1723bc32386
FH <2478923393@qq.com>
2022-07-26 21:28:02 +0800

    收益返还功能

---
+++ micr-task/src/main/java/com/bjpowernode/task/MicrTaskApplication.java
@@ -0,0 +1,22 @@
+package com.bjpowernode.task;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@EnableDubbo
+@SpringBootApplication
+public class MicrTaskApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = SpringApplication.run(MicrTaskApplication.class, args);
+        TaskManager tm = (TaskManager) ctx.getBean("taskManager");
+        //tm.taskIncomePlan();
+        tm.taskIncomeBack();
+    }
+
+}
